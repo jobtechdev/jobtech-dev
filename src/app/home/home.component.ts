3fdbe2239a0251213c3287d5482813a66c0a6310
Mats Löfstrand <mats.lofstrand@arbetsformedlingen.se>
2018-06-18 09:39:30 +0200

    index page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,38 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  images: Array<object>;
+
 
   constructor() {
   }
 
   ngOnInit() {
+    this.images = [
+    {
+      'header': 'Historical Job posting',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      'image': '../../assets/images/showcaseSlideShow/History.PNG',
+      'link': 'https://github.com/simonbe/afhistorik'
+    },
+
+    {
+      'header': 'Ontologiutforskaren',
+      'description': 'Lorem ipsum dolor sit amet.',
+      'image': '../../assets/images/showcaseSlideShow/Kopetensutforskaren.PNG',
+      'link': 'Show code',
+      'demolink': 'http://pilot.arbetsformedlingen.se/kompetensutforskaren/#!/'
+
+    },
+      {
+        'header': 'Job posting',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+        'image': '../../assets/images/showcaseSlideShow/JobPostings.PNG',
+        'link': 'https://github.com/jobtechdev/vacancieswidget',
+        'demolink': 'http://widget.northeurope.cloudapp.azure.com/kommun/vaxjo/'
+
+      }
+    ];
+    console.log(this.images)
   }
 }
