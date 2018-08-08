c2335e2d95e1c37bf1f5369a8404828a96102fff
Ulrika Häggqvist <ulrika.haggqvist@arbetsformedlingen.se>
2018-08-08 09:58:07 +0200

    Rättat år på historical card

--- src/app/shared/services/content.service.ts
+++ src/app/shared/services/content.service.ts
@@ -251,7 +251,7 @@ export const APIDATASETS = [
   {
     'name': 'historical-job-postings',
     'title': 'Historical Job Postings',
-    'shortDescription': 'All job postings from Platsbanken 2006-2016',
+    'shortDescription': 'All job postings from Platsbanken 2006-2017',
     'texts': [
       {
         'header': 'Description',
